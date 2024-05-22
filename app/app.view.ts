19569d514c70b9130a2368effdd51d90bbf38598
Lyumih <m.ivanov1225@gmail.com>
2024-05-22 12:33:12 +0300

    add turn

---
+++ app/app.view.ts
@@ -0,0 +1,28 @@
+namespace $.$$ {
+	/** 0 - белые, 1 - чёрные */
+	export class $go_app extends $.$go_app {
+
+		turn_label(): string {
+			return `Ход ${this.turns()}: ${this.turn() ? 'Белые' : 'Чёрные'}`
+		}
+
+		black_turn( next?: any ) {
+			if( !this.turn() ) {
+				this.turn( 1 )
+				this.turns_increment()
+			}
+		}
+
+		turns_increment() {
+			this.turns( this.turns() + 1 )
+		}
+
+		white_turn( next?: any ) {
+			if( this.turn() ) {
+				this.turn( 0 )
+				this.turns_increment()
+			}
+		}
+		
+	}
+}
